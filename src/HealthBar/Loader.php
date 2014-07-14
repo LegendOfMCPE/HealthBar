1fd5c18a7dce6d4fbe4ea8423a1ef7b1587ef974
iksaku <iksaku@me.com>
2014-07-13 22:20:53 -0500

    Now it will be disabled if there's an unknown style or position... More API :3

--- src/HealthBar/Loader.php
+++ src/HealthBar/Loader.php
@@ -1,18 +1,29 @@
 <?php
 namespace HealthBar;
 
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityRegainHealthEvent;
 use pocketmine\event\Listener;
-use pocketmine\event\entity;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 
 class Loader extends PluginBase implements Listener{
+    private $canRemove;
+
     public function onEnable(){
         $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        foreach($this->getServer()->getOnlinePlayers() as $player){
-            $this->updateHealthBar($player);
+        $this->checkConfiguration();
+    }
+
+    public function onDisable(){
+        if($this->canRemove === true){
+            foreach($this->getServer()->getOnlinePlayers() as $p){
+                $this->removeHealthBar($p);
+            }
         }
     }
 
@@ -23,45 +34,105 @@ class Loader extends PluginBase implements Listener{
         $this->updateHealthBar($event->getPlayer());
     }
 
-    public function onRegainHealth(entity\EntityRegainHealthEvent $event){
-        $player = $event->getEntity();
-        if($player instanceof Player){
-            $this->updateHealthBar($player);
+    public function onRegainHealth(EntityRegainHealthEvent $event){
+        $entity = $event->getEntity();
+        if($entity instanceof Player){
+            $this->updateHealthBar($entity);
         }
     }
 
-    public function onAttacked(entity\EntityDamagedByEntityEvent $event){
-        $player = $event->getEntity();
-        if($player instanceof Player){
-            $this->updateHealthBar($player);
+    public function onHealthLose(EntityDamageEvent $event){
+        $entity = $event->getEntity();
+        if($entity instanceof Player){
+            $this->updateHealthBar($entity);
         }
     }
 
-    public function onAttacked(entity\EntityDamageEvent $event){
-        $player = $event->getEntity();
-        if($player instanceof Player){
-            $this->updateHealthBar($player);
+    public function onAttack(EntityDamageByEntityEvent $event){
+        $entity = $event->getEntity();
+        if($entity instanceof Player){
+            $this->updateHealthBar($entity);
+        }
+    }
+
+    private function checkConfiguration(){
+        if(!$this->getStyle()){
+            $this->getLogger()->info(TextFormat::YELLOW . "[HealthBar] " . TextFormat::RED . "Unknown style given, HealthBar will be disabled!");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+            $this->setEnabled(false);
+            return false;
+        }elseif(!$this->getPosition()){
+            $this->getLogger()->info(TextFormat::YELLOW . "[HealthBar] " . TextFormat::RED . "Unknown position given, HealthBar will be disabled!");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+            $this->setEnabled(false);
+            return false;
+        }else{
+           $this->enableHealthBar();
+            return true;
+        }
+    }
+
+    private function enableHealthBar(){
+        $this->canRemove = true;
+        foreach($this->getServer()->getOnlinePlayers() as $p){
+            $this->updateHealthBar($p);
+        }
+    }
+
+    /*
+     *  .----------------.  .----------------.  .----------------.
+     * | .--------------. || .--------------. || .--------------. |
+     * | |      __      | || |   ______     | || |     _____    | |
+     * | |     /  \     | || |  |_   __ \   | || |    |_   _|   | |
+     * | |    / /\ \    | || |    | |__) |  | || |      | |     | |
+     * | |   / ____ \   | || |    |  ___/   | || |      | |     | |
+     * | | _/ /    \ \_ | || |   _| |_      | || |     _| |_    | |
+     * | ||____|  |____|| || |  |_____|     | || |    |_____|   | |
+     * | |              | || |              | || |              | |
+     * | '--------------' || '--------------' || '--------------' |
+     *  '----------------'  '----------------'  '----------------'
+     *
+     */
+
+    public function getStyle(){
+        $style = $this->getConfig()->get("style");
+        if($style == "default"){
+            return "default";
+        }else{
+            return false;
+        }
+    }
+
+    public function getPosition(){
+        $position = $this->getConfig()->get("position");
+        if($position == "above"){
+            return "above";
+        }elseif($position == "under"){
+            return "under";
+        }else{
+            return false;
         }
     }
 
     public function updateHealthBar(Player $player){
-        $config = $this->getConfig();
-        $style = $config->get("style");
-        $position = $config->get("position");
+        $style = $this->getStyle();
+        $position = $this->getPosition();
+
         if($style == "default"){
             $style = $player->getHealth() . "/" . $player->getMaxHealth();
-        }else{
-            $this->getLogger()->error("[HealthBar] Unknown style given. Player health bars will not be updated.");
-            return false;
         }
+
         if($position == "above"){
             $player->setNameTag($style . "\n" . $player->getDisplayName());
         }elseif($position == "under"){
             $player->setNameTag($player->getDisplayName() . "\n" . $style);
-        }else{
-            $this->getLogger()->error("[HealthBar] Unknown position given. Player health bars will not be updated.");
-            return false;
         }
         return true;
+    }
+
+    public function removeHealthBar(Player $player){
+        if($this->canRemove === true){
+            $player->setNameTag($player->getDisplayName());
+        }
     }
 }
